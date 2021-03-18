29c813ac45721ebd3a97d07dd25d509c8ba77a8b
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2021-03-19 07:57:57 +0900

    slick slider function

--- js/main.js
+++ js/main.js
@@ -5,4 +5,23 @@
       $('.menu-btn, #nav, #mask').toggleClass('open');
     });
   });
+
+  $(function () {
+    $('.center').slick({
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 3,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '60px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+  });
 }
