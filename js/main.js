0b41e7f6e72a3194b69e3e2ae226e6c194a73072
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2021-03-19 18:27:16 +0900

    add comments

--- js/main.js
+++ js/main.js
@@ -48,12 +48,13 @@
     スムーススクロール
     ===================================================*/
     $('a[href^="#"]').click(function(){
-        let speed = 500;
-        let href= $(this).attr("href");
-        let target = $(href == "#" || href == "" ? 'html' : href);
-        let position = target.offset().top;
-        $("html, body").animate({scrollTop:position}, speed, "swing");
-        return false;
+      let speed = 500;
+      let href= $(this).attr("href");
+      let target = $(href == "#" || href == "" ? 'html' : href);
+      let position = target.offset().top;
+      $("html, body").animate({scrollTop:position}, speed, "swing");
+      return false;
+      // speedは直接引数に入れても良さそう
     });
   });
 }
