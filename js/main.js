ea74582a3cdfc94dfeb099acd6a3dccb5e640e32
Satomaru178 <satoshichangetheworld@yahoo.co.jp>
2021-03-19 17:30:35 +0900

    fadein function

--- js/main.js
+++ js/main.js
@@ -32,6 +32,18 @@
     /*=================================================
     フェードイン
     ===================================================*/
+    $(window).scroll(function (){
+      $('.fadein').each(function(){
+        var targetElement = $(this).offset().top;
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll > targetElement - windowHeight){
+          $(this).css('opacity','1');
+          $(this).css('transform','translateY(0)');
+        }
+      });
+    });
+
     /*=================================================
     スムーススクロール
     ===================================================*/
